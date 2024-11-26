b017182961a7345e40ed2d02a8b950ecdec67302
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-26 09:57:15 +0600

    delete operation

--- Espresso-Emporium-Client/src/components/CoffeeCard.jsx
+++ Espresso-Emporium-Client/src/components/CoffeeCard.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import { FaEye, FaPen, FaTrash } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
   const { _id, name, photo, chef } = coffee;
+
+  const handleDelete = id =>{
+    console.log(id)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+
+        fetch(`http://localhost:5000/coffee/${id}`, {
+          method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          if(data.deletedCount > 0){
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your coffee has been deleted.",
+              icon: "success"
+            });
+          }
+        })
+      }
+    });
+  }
+
   return (
     <div className="flex items-center justify-between gap-6 border p-8">
       <div>
@@ -22,16 +56,16 @@ const CoffeeCard = ({ coffee }) => {
             <span className="font-semibold">Price: </span>$69
           </p>
         </div>
-        <div className="space-y-2">
-          <div className="p-2 text-white text-sm cursor-pointer bg-[#D2B48C] rounded-md">
+        <div className="space-y-2 flex flex-col">
+          <button className="p-2 text-white text-sm cursor-pointer bg-[#D2B48C] rounded-md">
             <FaEye />
-          </div>
-          <div className="p-2 text-white text-sm cursor-pointer bg-[#3C393B] rounded-md">
+          </button>
+          <button className="p-2 text-white text-sm cursor-pointer bg-[#3C393B] rounded-md">
             <FaPen />
-          </div>
-          <div className="p-2 text-white text-sm cursor-pointer bg-[#EA4744] rounded-md">
+          </button>
+          <button onClick={()=>handleDelete(_id)} className="p-2 text-white text-sm cursor-pointer bg-[#EA4744] rounded-md">
             <FaTrash />
-          </div>
+          </button>
         </div>
       </div>
     </div>
