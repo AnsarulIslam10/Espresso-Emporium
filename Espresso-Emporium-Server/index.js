79f9f23a5a70344d0f95c88468476fa268256c79
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-29 23:45:08 +0600

    update user login info after successfull login

--- Espresso-Emporium-Server/index.js
+++ Espresso-Emporium-Server/index.js
@@ -29,6 +29,7 @@ async function run() {
 
     const coffeeCollection = client.db("coffeeDB").collection('coffee')
     
+    const userCollection = client.db('coffeeDB').collection('users')
 
     // create
     app.get('/coffee', async(req, res)=>{
@@ -79,6 +80,43 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    // Users related apis
+    app.get('/users', async(req, res)=>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/users', async(req, res)=>{
+      const newUser = req.body;
+      console.log(newUser)
+      const result = await userCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+
+    //update user login info after successfull login
+    app.patch('/users', async(req, res)=>{
+      const email = req.body.email;
+      const filter = {email};
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    // delete
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
